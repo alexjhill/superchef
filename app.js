6182fab9f50383b21bc93f523e0690db88eaa24d
Alex Hill <alexhillh@gmail.com>
2018-03-28 18:30:40 +0100

    User auth, session storage, session routing

--- app.js
+++ app.js
@@ -1,32 +1,107 @@
-var createError = require('http-errors');
+// browser-sync start --proxy "localhost:8000" --files "views/partials/*.hbs, views/*.hbs, public/css/*.css"
+
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
-var hbs = require('express-handlebars');
-var expressValidator = require('express-validator');
-var expressSession = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+
+
+// MONGODB SETUP
+var mongoose = require('mongoose');
+var User = require('./models/user');
+
+mongoose.connect('mongodb://alexjhill:password@ds121118.mlab.com:21118/linear');
+var db = mongoose.connection;
+
+// Check connection
+db.once ('open', function() {
+    console.log("Connected to MongoDB");
+});
+
+// Check for db errors
+db.on ('error', function(err){
+    console.log(err);
+});
+
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
+
+// AUTHENTICATION SETUP
+var expressValidator = require('express-validator');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var MongoStore = require('connect-mongo')(session);
+var bcrypt = require('bcrypt');
+
 
 var app = express();
 
+
 // view engine setup
-app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __dirname + "/views/layouts/"}));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(expressSession({secret: 'alex', saveUninitialised: false, resave: false}));
+
+app.use(session({
+    secret: 'agsdhsdfqeqefvsdvsdv',
+    resave: false,
+    saveUninitialized: false,
+    // cookie: { secure: true }
+    store: new MongoStore({ mongooseConnection: db }),
+    cookie: { maxAge: 180 * 60 * 1000 }
+}))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(function(req, res, next) {
+    res.locals.isAuthenticated = req.isAuthenticated();
+    next();
+});
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+
+        User.findOne({ username: username }, function (err, user) {
+            if (err) { return done(err); }
+            if (!user) {
+                return done(null, false);
+            } else {
+                const hash = user.password;
+
+                bcrypt.compare(password, hash, function(err, response){
+                    if (response === true) {
+                        return done(null, {user_id: user._id});
+                    } else {
+                        return done(null, false);
+                    }
+                });
+            }
+
+
+        })
+    }
+));
+
+
+app.use(function(req, res, next) {
+    res.locals.login = req.isAuthenticated();
+    res.locals.session = req.session;
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -43,5 +118,42 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error');
 });
+
+
+// Handlebars default config
+const hbs = require('hbs');
+const fs = require('fs');
+
+
+
+
+const partialsDir = __dirname + '/views/partials';
+const partialsFiles = fs.readdirSync(partialsDir);
+
+partialsFiles.forEach(function (filename) {
+    const matches = /^([^.]+).hbs$/.exec(filename);
+    if (!matches) {
+        return;
+    }
+    const name = matches[1];
+    const template = fs.readFileSync(partialsDir + '/' + filename, 'utf8');
+    hbs.registerPartial(name, template);
+});
+
+hbs.registerHelper('json', function(context) {
+    return JSON.stringify(context, null, 2);
+});
+
+
+
+const modelsDir = __dirname + '/models';
+const modelsFiles = fs.readdirSync(modelsDir);
+
+modelsFiles.forEach(function (filename) {
+    if (~filename.indexOf('.js')) require(modelsDir + '/' + filename);
+});
+
+
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,128 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Form Validation', success: req.session.success, errors: req.session.errors });
-    req.session.errors = null;
+var mongoose = require('mongoose');
+var User = require('../models/user');
+
+var expressValidator = require('express-validator');
+var passport = require('passport');
+
+var bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+router.get('/', function(req, res){
+    console.log(req.user);
+    console.log(req.isAuthenticated());
+    res.render('home', { title: 'Home' });
 });
 
-router.post('/submit', function(req, res, next) {
-    // Check validity
-    req.check('email', 'Invalid email address').isEmail();
-    req.check('password', 'Invalid password').isLength({min: 6});
 
-    var errors = req.validationErrors();
-    if (errors) {
-        req.session.errors = errors
-        req.session.seccess = false;
-    } else {
-        req.session.success = true;
-    }
+router.get('/profile', authenticationMiddleware(), function(req, res) {
+    res.render('profile', { title: 'Profile' });
+});
+
+
+// Login
+router.get('/login', function(req, res) {
+    res.render('login', { title: 'Login' });
+});
+
+
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/profile',
+    failureRedirect: '/login'
+}));
+
+
+
+// Logout
+router.get('/logout', function(req, res) {
+    req.logout();
+    req.session.destroy();
     res.redirect('/');
 });
+
+
+
+
+
+// Register
+router.get('/register', function(req, res) {
+    res.render('register', { title: 'Registration' });
+});
+
+router.post('/register', function(req, res, next) {
+
+    req.checkBody('name', 'Name field cannot be empty.').notEmpty();
+    req.checkBody('name', 'Name field must be between 4-15 characters long.').len(4, 15);
+
+    req.checkBody('email', 'The email you entered is invalid, please try again.').isEmail();
+    req.checkBody('email', 'Email address must be between 5-100 characters long, please try again.').len(5, 100);
+
+    req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+    req.checkBody('username', 'Username must be between 4-15 characters long.').len(4, 15);
+
+    req.checkBody('password', 'Password must be between 8-100 characters long.').len(8, 100);
+    req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and a special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+
+    const errors = req.validationErrors();
+
+    if (errors) {
+        console.log('errors, ${JSON.stringify(errors)}');
+
+
+        res.render('register', {
+            title: 'Registration Error',
+            errors: errors
+        });
+    } else {
+
+        var myName = req.body.name;
+        var myEmail = req.body.email;
+        var myUsername = req.body.username;
+        var myPassword = req.body.password;
+
+
+        bcrypt.hash(myPassword, saltRounds, function(err, hash) {
+
+            var user = new User ({
+                name: myName,
+                email: myEmail,
+                username: myUsername,
+                password: hash
+            });
+
+            user.save(function(error, myUser) {
+                if (error) throw error;
+
+                const user_id = myUser._id;
+
+                console.log('user created!');
+
+                req.login(user_id, function(err) {
+                    res.redirect('/');
+                });
+            });
+        });
+    }
+});
+
+passport.serializeUser(function(user_id, done) {
+    done(null, user_id);
+});
+
+passport.deserializeUser(function(user_id, done) {
+    done(null, user_id);
+});
+
+function authenticationMiddleware () {
+    return (req, res, next) => {
+        console.log(`req.session.passport.user: ${JSON.stringify(req.session.passport)}`);
+
+        if (req.isAuthenticated()) return next();
+        res.redirect('/login')
+    }
+}
 
 module.exports = router;
